388bf0dd7de50bf2bca00ad13c4883f47f6f093d
enterspacebs <anotherhell48@gmail.com>
2023-09-16 16:15:24 +0900

    model made

--- jimgabang/models/reservations.py
+++ jimgabang/models/reservations.py
@@ -5,14 +5,20 @@ from typing import Optional, List
 
 
 # models 폴더의 모델 파일을 변경하여 몽고DB 문서를 사용할 수 있도록 한다.
-class Event(Document):
+class Opening(Document):
     # id: int
-    creator: Optional[str]  # 해당 이벤트를 소유한 사용자만 처리할 수 있도록 한다.
-    title: str
-    image: str
-    description: str
+    creator: Optional[str]  # 해당 서비스를 소유한 사용자만 처리할 수 있도록 한다.
+    seviceName: str
+    category: str
+    address: str
+    latitude: float
+    longitude: float
+    openingTime: str
+    openingDate: List[str]
     tags: List[str]
-    location: str
+    tags: List[str]
+    availableBag: int
+    totalAvailableBag: int
 
     # model_config = {
     #     "json_schema_extra": {
@@ -28,10 +34,34 @@ class Event(Document):
         }
 
     class Settings:  # Beanie ORM을 사용하여 MongoDB에 저장할 때 사용할 설정을 정의
-        name = "events"  # Event 모델을 사용하여 MongoDB에 저장할 때 사용할 컬렉션 이름을 정의. 기본값은 모델 이름의 소문자 복수형이다. 사용자 컬렉션을 사용하려면 이 값을 설정해야 한다.
+        name = "openings"  # Event 모델을 사용하여 MongoDB에 저장할 때 사용할 컬렉션 이름을 정의. 기본값은 모델 이름의 소문자 복수형이다. 사용자 컬렉션을 사용하려면 이 값을 설정해야 한다.
 
 
-class EventUpdate(BaseModel):
+class Booking(Document):
+    # id: int
+    creator: Optional[str]  # 해당 이벤트를 소유한 사용자만 처리할 수 있도록 한다.
+    bookDate: List[str]
+    bookingBag: int
+    confirm: bool
+
+    # model_config = {
+    #     "json_schema_extra": {
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet",
+            }
+        }
+
+    class Settings:  # Beanie ORM을 사용하여 MongoDB에 저장할 때 사용할 설정을 정의
+        name = "bookings"  # Event 모델을 사용하여 MongoDB에 저장할 때 사용할 컬렉션 이름을 정의. 기본값은 모델 이름의 소문자 복수형이다. 사용자 컬렉션을 사용하려면 이 값을 설정해야 한다.
+
+
+class OpeningUpdate(BaseModel):
     title: Optional[str]
     image: Optional[str]
     description: Optional[str]
@@ -48,3 +78,73 @@ class EventUpdate(BaseModel):
                 "location": "Google Meet",
             }
         }
+
+
+class BookingUpdate(BaseModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet",
+            }
+        }
+
+
+# from pydantic import BaseModel
+# from beanie import Document
+# from typing import Optional, List
+
+
+# # models 폴더의 모델 파일을 변경하여 몽고DB 문서를 사용할 수 있도록 한다.
+# class Event(Document):
+#     # id: int
+#     creator: Optional[str]  # 해당 이벤트를 소유한 사용자만 처리할 수 있도록 한다.
+#     title: str
+#     image: str
+#     description: str
+#     tags: List[str]
+#     location: str
+
+#     # model_config = {
+#     #     "json_schema_extra": {
+#     class Config:
+#         schema_extra = {
+#             "example": {
+#                 "title": "FastAPI Book Launch",
+#                 "image": "https://linktomyimage.com/image.png",
+#                 "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+#                 "tags": ["python", "fastapi", "book", "launch"],
+#                 "location": "Google Meet",
+#             }
+#         }
+
+#     class Settings:  # Beanie ORM을 사용하여 MongoDB에 저장할 때 사용할 설정을 정의
+#         name = "events"  # Event 모델을 사용하여 MongoDB에 저장할 때 사용할 컬렉션 이름을 정의. 기본값은 모델 이름의 소문자 복수형이다. 사용자 컬렉션을 사용하려면 이 값을 설정해야 한다.
+
+
+# class EventUpdate(BaseModel):
+#     title: Optional[str]
+#     image: Optional[str]
+#     description: Optional[str]
+#     tags: Optional[List[str]]
+#     location: Optional[str]
+
+#     class Config:
+#         schema_extra = {
+#             "example": {
+#                 "title": "FastAPI Book Launch",
+#                 "image": "https://linktomyimage.com/image.png",
+#                 "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+#                 "tags": ["python", "fastapi", "book", "launch"],
+#                 "location": "Google Meet",
+#             }
+#         }
--- jimgabang/models/users.py
+++ jimgabang/models/users.py
@@ -5,15 +5,42 @@ from pydantic import BaseModel, EmailStr
 from models.events import Event
 
 
-class User(Document):
+class Server(Document):
     email: EmailStr
     password: str
+    serverName: str
+    image: str
+    # opening: Optional[List[Opening]]
     # events: Optional[
     #     List[Event]
     # ]  # 선택적 필드인 Optional은 필요에 따라 값을 포함하거나 생략할 수 있다. None이 될 수 있다. # = None  # 기본값을 정해줘야 한다.
 
     class Settings:  # Beanie ORM을 사용하여 MongoDB에 저장할 때 사용할 설정을 정의
-        name = "users"  # User 모델을 사용하여 MongoDB에 저장할 때 사용할 컬렉션 이름을 정의. 기본값은 모델 이름의 소문자 복수형이다. 사용자 컬렉션을 사용하려면 이 값을 설정해야 한다.
+        name = "servers"  # User 모델을 사용하여 MongoDB에 저장할 때 사용할 컬렉션 이름을 정의. 기본값은 모델 이름의 소문자 복수형이다. 사용자 컬렉션을 사용하려면 이 값을 설정해야 한다.
+
+    # model_config = {
+    #     "json_schema_extra": {
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                # "events": [],
+            }
+        }
+
+
+class Client(Document):
+    email: EmailStr
+    password: str
+    clientName: str
+    # booking: Optional[List[Booking]]
+    # events: Optional[
+    #     List[Event]
+    # ]  # 선택적 필드인 Optional은 필요에 따라 값을 포함하거나 생략할 수 있다. None이 될 수 있다. # = None  # 기본값을 정해줘야 한다.
+
+    class Settings:  # Beanie ORM을 사용하여 MongoDB에 저장할 때 사용할 설정을 정의
+        name = "clients"  # User 모델을 사용하여 MongoDB에 저장할 때 사용할 컬렉션 이름을 정의. 기본값은 모델 이름의 소문자 복수형이다. 사용자 컬렉션을 사용하려면 이 값을 설정해야 한다.
 
     # model_config = {
     #     "json_schema_extra": {
